2c6564c72b7f0965a11cb256eb9e8b5376428d66
freepk <freepk@mail.ru>
2018-04-04 21:35:37 +0000

    fmt

--- union.go
+++ union.go
@@ -1,61 +1,61 @@
 package iterator
 
 type UnionIterator struct {
-        iterators []Iterator
-        values    []int
-        remains   int
+	iterators []Iterator
+	values    []int
+	remains   int
 }
 
 func NewUnionIterator(iterators []Iterator) *UnionIterator {
-        size := len(iterators)
-        iter := &UnionIterator{
-                iterators: iterators,
-                values:    make([]int, size),
-                remains:   size}
-        return iter
+	size := len(iterators)
+	iter := &UnionIterator{
+		iterators: iterators,
+		values:    make([]int, size),
+		remains:   size}
+	return iter
 }
 
 func (it *UnionIterator) swap(offset int) {
-        it.remains--
-        it.iterators[it.remains], it.iterators[offset] = it.iterators[offset], it.iterators[it.remains]
-        it.values[it.remains], it.values[offset] = it.values[offset], it.values[it.remains]
+	it.remains--
+	it.iterators[it.remains], it.iterators[offset] = it.iterators[offset], it.iterators[it.remains]
+	it.values[it.remains], it.values[offset] = it.values[offset], it.values[it.remains]
 }
 
 func (it *UnionIterator) first() {
-        ok := false
-        for i := 0; i < it.remains; i++ {
-                it.values[i], ok = it.iterators[i].Next()
-                if !ok {
-                        it.swap(i)
-                }
-        }
+	ok := false
+	for i := 0; i < it.remains; i++ {
+		it.values[i], ok = it.iterators[i].Next()
+		if !ok {
+			it.swap(i)
+		}
+	}
 }
 
 func (it *UnionIterator) Reset() {
-        it.remains = len(it.iterators)
-        for i := 0; i < it.remains; i++ {
-                it.iterators[i].Reset()
-        }
-        it.first()
+	it.remains = len(it.iterators)
+	for i := 0; i < it.remains; i++ {
+		it.iterators[i].Reset()
+	}
+	it.first()
 }
 
 func (it *UnionIterator) Next() (int, bool) {
-        if it.remains == 0 {
-                return 0, false
-        }
-        offset := 0
-        advice := it.values[0]
-        for i := 1; i < it.remains; i++ {
-                if advice > it.values[i] {
-                        offset = i
-                        advice = it.values[i]
-                }
-        }
-        value, ok := it.iterators[offset].Next()
-        if ok {
-                it.values[offset] = value
-        } else {
-                it.swap(offset)
-        }
-        return advice, true
+	if it.remains == 0 {
+		return 0, false
+	}
+	offset := 0
+	advice := it.values[0]
+	for i := 1; i < it.remains; i++ {
+		if advice > it.values[i] {
+			offset = i
+			advice = it.values[i]
+		}
+	}
+	value, ok := it.iterators[offset].Next()
+	if ok {
+		it.values[offset] = value
+	} else {
+		it.swap(offset)
+	}
+	return advice, true
 }

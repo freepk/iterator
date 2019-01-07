7626b0908a5f17dac0e8bbf0aa0bfbb099904902
freepk <freepk@mail.ru>
2019-01-07 22:07:33 +0300

    Update union.go

--- union.go
+++ union.go
@@ -3,7 +3,7 @@ package iterator
 type UnionIterator struct {
 	a []Iterator
 	v []int
-	p int
+	n int
 }
 
 func NewUnionIterator(a []Iterator) *UnionIterator {
@@ -15,31 +15,18 @@ func NewUnionIterator(a []Iterator) *UnionIterator {
 func (it *UnionIterator) first() {
 	it.v = it.v[:0]
 	n := len(it.a)
-	m := 0
 	i := 0
 	for i < n {
 		v, ok := it.a[i].Next()
-		if ok {
-			j := 0
-			for j < m {
-				if v < it.v[j] {
-					break
-				}
-				j++
-			}
-			m++
-			it.v = append(it.v, 0)
-			copy(it.v[j+1:], it.v[j:])
-			it.v[j] = v
-			it.a[i], it.a[j] = it.a[j], it.a[i]
-			i++
-		} else {
+		if !ok {
 			n--
 			it.a[i] = it.a[n]
+			continue
 		}
+		it.v = append(it.v, v)
+		i++
 	}
-	it.a = it.a[:n]
-	it.p = 0
+	it.n = n
 }
 
 func (it *UnionIterator) Reset() {
@@ -51,26 +38,26 @@ func (it *UnionIterator) Reset() {
 }
 
 func (it *UnionIterator) Next() (int, bool) {
-	n := len(it.a)
-	p := it.p
-	if p >= n {
+	if it.n == 0 {
 		return 0, false
 	}
-	v := it.v[p]
-	x, ok := it.a[p].Next()
-	if ok {
-		it.v[p] = x
-		p++
-		for p < n {
-			if x < it.v[p] {
-				break
-			}
-			it.a[p-1], it.a[p] = it.a[p], it.a[p-1]
-			it.v[p-1], it.v[p] = it.v[p], it.v[p-1]
-			p++
+	i := 0
+	v := it.v[0]
+	for j := 1; j < it.n; j++ {
+		if v > it.v[j] {
+			i = j
+			v = it.v[j]
 		}
+	}
+	x, ok := it.a[i].Next()
+	if ok {
+		it.v[i] = x
 	} else {
-		it.p++
+		it.n--
+		if i != it.n {
+			it.a[i], it.a[it.n] = it.a[it.n], it.a[i]
+			it.v[i], it.v[it.n] = it.v[it.n], it.v[i]
+		}
 	}
 	return v, true
 }

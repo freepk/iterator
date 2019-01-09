d3debd6fbf02ea1b2169aa506103e974dadd2e4e
freepk <freepk@mail.ru>
2019-01-09 05:57:44 +0300

    KISS

--- iterator.go
+++ iterator.go
@@ -1,35 +1,6 @@
 package iterator
 
-import (
-	"errors"
-)
-
-var (
-	iterInvalidParamErr = errors.New("Invalid parameter")
-)
-
 type Iterator interface {
 	Reset()
 	Next() (int, bool)
-}
-
-type unionIter struct {
-}
-
-func newUnionIter(a Iterator, b Iterator) Iterator {
-	return nil
-}
-
-type unionAllIter struct {
-}
-
-func newUnionAllIter(a Iterator, b Iterator) Iterator {
-	return nil
-}
-
-type exceptIter struct {
-}
-
-func newExceptIter(a Iterator, b Iterator) Iterator {
-	return nil
 }
